c0f316c3d0e12c2d32305da74d4eae4a37716612
pasztetblr <paxanas18@gmail.com>
2021-12-17 21:23:49 +0300

    changed stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require("../extensions/index.js");
+//const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -12,19 +12,22 @@ const { NotImplementedError } = require("../extensions/index.js");
  * stack.pop(); // undefined
  *
  */
-module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+ module.exports = class Stack {
+  constructor(){
+    this.item=[];
+  }
+  push(element) {
+    this.item.push(element);
   }
 
   pop() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    if(this.item.length==0){
+      return "undefined";
+    }
+    return this.item.pop();
   }
 
   peek() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    return this.item[this.item.length-1];
   }
 };
