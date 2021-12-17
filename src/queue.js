30fe5bd50b0675c8f6b3cc9b1462851868326615
pasztetblr <paxanas18@gmail.com>
2021-12-17 19:34:55 +0300

    проблема решена

---
+++ src/queue.js
@@ -0,0 +1,50 @@
+//const { NotImplementedError } = require('../extensions/index.js');
+
+const { ListNode } = require('../extensions/list-node.js');
+
+/**
+ * Implement the Queue with a given interface via linked list (use ListNode extension above).
+ *
+ * @example
+ * const queue = new Queue();
+ *
+ * queue.enqueue(1); // adds the element to the queue
+ * queue.enqueue(3); // adds the element to the queue
+ * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+ * queue.getUnderlyingList() // returns { value: 3, next: null }
+ */
+module.exports = class Queue {
+  constructor(){
+    this.head = null;
+    this.tail = null;
+  }
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    const newNode= new ListNode(value);
+    if(!this.head || !this.tail){
+      this.head =newNode;
+      this.tail = newNode;
+      return this;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    return this;
+  }
+
+  dequeue() {
+    if(!this.head){
+      return null;
+    }
+    const deleteHead= this.head;
+    if(this.head.next){
+      this.head=this.head.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+    return deleteHead.value;
+  }
+}
---
+++ src/remove-from-list.js
@@ -0,0 +1,43 @@
+//const { NotImplementedError } = require('../extensions/index.js');
+
+/**
+ * Given a singly linked list of integers l and an integer k,
+ * remove all elements from list l that have a value equal to k.
+ *
+ * @param {List} l
+ * @param {Number} k
+ * @return {List}
+ *
+ * @example
+ * For l = [3, 1, 2, 3, 4, 5] and k = 3,
+ * the output should be [1, 2, 4, 5]
+ *
+ * Singly - linked lists are already defined with this interface
+ * function ListNode(x) {
+ *   this.value = x;
+ *   this.next = null;
+ * }
+ */
+ 
+ module.exports = function removeKFromList(l, k) {
+  let head = l
+  let previous=null
+  while(l){
+    if(l.value===k){
+      if(previous=== null){
+        head=l=l.next;
+        continue;
+      } 
+      else {
+        previous.next=l.next;
+        l=l.next;
+        continue;
+      }
+    } 
+    else {
+      previous = l;
+      l=l.next;
+    }
+  }
+  return head;
+}
